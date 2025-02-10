bdc0938ba1f75e82867d969b66b9dda363d2beff
pawan kumar ghashal <ghashalpawan@gmail.com>
2025-02-10 16:24:36 +0530

    Add Project Files

---
+++ utils/ExpressError.js
@@ -0,0 +1,14 @@
+
+ class ExpressError extends Error{
+    constructor(statusCode,message){
+        super(message);
+        this.statusCode=statusCode;
+    }
+
+ }
+
+
+
+
+ module.exports = ExpressError ;
+ 
